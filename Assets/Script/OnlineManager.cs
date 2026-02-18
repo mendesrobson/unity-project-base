74b81e4a420aaf30cbeaed170f0feb2d0fa071e3
Robson Mendes <robsonmendes_1@outlook.com>
2026-02-18 02:38:28 -0300

    feat: create lobby

--- Assets/Script/OnlineManager.cs
+++ Assets/Script/OnlineManager.cs
@@ -4,57 +4,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 
 public class NewMonoBehaviourScript : MonoBehaviourPunCallbacks
 {
+    public GameObject painelLogin, PainelRoom, background;
+    public InputField playerName, roomName;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        painelLogin.SetActive(false);
+        PainelRoom.SetActive(false);
+        background.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.eKey.wasPressedThisFrame)
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
         {
-            PhotonNetwork.Disconnect();
+            //PhotonNetwork.Disconnect();
+            painelLogin.SetActive(!painelLogin.activeSelf);
+            background.SetActive(!background.activeSelf);
         }
+    }
+
+    public void Login()
+    {
+        PhotonNetwork.NickName = playerName.text;
+        PhotonNetwork.ConnectUsingSettings();
+        painelLogin.SetActive(false);
+        PainelRoom.SetActive(true);
+    }
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.JoinOrCreateRoom(roomName.text, new RoomOptions { MaxPlayers = 10 }, TypedLobby.Default);
+        painelLogin.SetActive(false);
+        PainelRoom.SetActive(false);
+        background.SetActive(false);
     }
 
     // CONECTADO
     public override void OnConnectedToMaster()
     {
-        //base.OnConnectedToMaster();
         PhotonNetwork.JoinLobby();
         Debug.Log("Connected to Master");
     }
 
     public override void OnJoinedLobby()
     {
-        //base.OnJoinedLobby();
-        Debug.Log("Joined Lobby");
-        PhotonNetwork.JoinRandomRoom();        
+        Debug.Log("Joined Lobby");      
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
         Debug.Log("Disconnected from Master: " + cause.ToString());
-        // base.OnDisconnected(cause);
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        // base.OnJoinRandomFailed(returnCode, message);
         Debug.Log("Failed to join random room: " + message);
-        //PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 10 });
-        PhotonNetwork.CreateRoom(null, new RoomOptions { });
     }
 
     public override void OnJoinedRoom()
     {
-        Debug.Log("Joined room: " + PhotonNetwork.CurrentRoom.Name);
-        //base.OnJoinedRoom();
+        Debug.Log("Joined room! Spawnig player....");
+        print(PhotonNetwork.CurrentRoom.Name);
+        print(PhotonNetwork.CurrentRoom.PlayerCount);
+        print(PhotonNetwork.NickName);
     }
 }
